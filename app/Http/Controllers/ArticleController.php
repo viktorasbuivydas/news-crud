556d71fafa5a7bad93b0dd9db47bff77b03218f5
Viktoras Buivydas <viktorasss00@gmail.com>
2026-03-23 11:05:01 +0200

    adds basic Article controller, web route, blade layout

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('articles.index');
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
+Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
